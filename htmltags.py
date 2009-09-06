0c27facd72e68694f1fb5e63dda71c495bcb380e
Philip Dorrell <http://www.1729.com/email.html>
2009-09-06 21:17:51 +1200

    use globals() to define tag functions from a list of tag strings

--- htmltags.py
+++ htmltags.py
@@ -1,23 +1,28 @@
 
-def _toString(data): 
+def _toString(data):
+    """Return string form of object,  _unless_ it is a tuple or list, in which
+    case concatenate string forms of items in the tuple/list."""
     if type(data) in [tuple, list]:
         return "".join([_toString(item) for item in data])
     else:
         return str(data)
 
 class Tag:
+    """Class representing an HTML tag element. Has name, child elements, and attributes."""
     def __init__(self, name, *children, **attributes):
         self.name = name
         self.children = children
         self.attributes = attributes
         
     def __str__(self):
+        """How this tag is output as HTML text."""
         return self.startString() + _toString(self.children) + self.endString()
     
     def __repr__(self):
         return str(self)
     
     def startString(self):
+        """Output HTML string for opening tag for this element"""
         if len(self.attributes) == 0:
             attributesString = ""
         else:
@@ -26,17 +31,19 @@ class Tag:
         return "<%s%s>" % (self.name, attributesString)
     
     def endString(self):
+        """Output HTML string for closing tag for this element"""
         return "</%s>" % self.name
     
 def tagFunction(name):
+    """Return a function for creating a Tag object with specified name"""
     def func(*children, **attributes): 
         return Tag(name, *children, **attributes)
     return func
-    
-H2 = tagFunction("h2")
-A = tagFunction("a")
-P = tagFunction("p")
-B = tagFunction("b")
-UL = tagFunction("ul")
-LI = tagFunction("li")
-SMALL = tagFunction("small")
+
+"""List of HTML tags (incomplete at the moment)"""
+htmlTagNames = ["h2", "a", "p", "b", "ul", "li", "small"]
+
+"""Define tag functions for names in htmlTagNames (function names 
+are capitalized, e.g. UL for <ul> tag)."""
+for tagName in htmlTagNames:
+    globals()[tagName.upper()] = tagFunction(tagName)
